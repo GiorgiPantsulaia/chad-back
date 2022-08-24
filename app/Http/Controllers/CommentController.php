7788434bfe97461f2e0b49a9df41cb5beff80133
Giorgi Pantsulaia <pancul1211@gmail.com>
2022-08-24 22:40:04 +0400

    feat:add broadcasting for deleting comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Events\NewNotification;
 use App\Events\PostCommented;
+use App\Events\CommentDeleted;
 use App\Http\Requests\CreateCommentRequest;
 use App\Http\Requests\NotificationRequests\CommentNotificationRequest;
 use App\Models\Comment;
@@ -33,7 +34,10 @@ class CommentController extends Controller
 
 	public function destroy(Comment $comment)
 	{
+		event(new CommentDeleted($comment));
+
 		$comment->delete();
-		return response()->json(['message'=>'Comment deleted successfully.'], 200);
+
+		return response()->json(['message'=>'Comment deleted successfully'], 200);
 	}
 }
